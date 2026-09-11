14bee7e6b5fc93ca0c4543f45b107a1a90cd4ae8
SANMIAO0101 <2772276265@qq.com>
2026-09-11 21:45:42 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.RA_CONFIG = window.RA_CONFIG || {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
-  demoMode: true,
+  supabaseUrl: 'https://bylcqvxfzqxekactjzfa.supabase.co',
+  supabaseAnonKey: 'sb_publishable_UgpOPndIULFS3psf2XxCHw_IUgFHBBc',
+  demoMode: false,
   appVersion: '3.0.0'
 };
